4d8b54e3fa5d9d3032567b5fa60a90e364419dd9
Kamalalhagh <kamalalhagh@gmail.com>
2023-07-26 19:52:00 +1000

    create simple game of connect4

---
+++ game/connect4/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Connect4Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'connect4'
---
+++ game/connect4/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "game/index.html")
+# Create your views here.
